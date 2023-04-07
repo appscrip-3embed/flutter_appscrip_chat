dabd6b4691439b946a6aa6c54b5e3de443da7c58
rahulsaryam <100126041+rahulsaryam@users.noreply.github.com>
2023-04-07 18:56:44 +0530

    feat(mqtt connection in progress)

--- lib/src/controllers/mqtt/mqtt_controller.dart
+++ lib/src/controllers/mqtt/mqtt_controller.dart
@@ -57,6 +57,9 @@ class IsmChatMqttController extends GetxController {
 
   void connectClient() async {
     try {
+      IsmChatLog.success('userName  :- ${_communicationConfig.username}');
+      IsmChatLog.success('password  :- ${_communicationConfig.password}');
+
       var res = await client.connect(
         _communicationConfig.username,
         _communicationConfig.password,
