5346e19cfcda72dd219491cb67a0c594002f5639
Ajay Thakur <ajay@appscrip.co>
2023-04-26 14:01:45 +0530

    feat (assets, versioning ) : add the assets and versioning for ios build

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -7,6 +7,9 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      if #available(iOS 10.0, *) {
+       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+     }
     GMSServices.provideAPIKey("AIzaSyC2YXqs5H8QSfN1NVsZKsP11XLZhfGVGPI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
