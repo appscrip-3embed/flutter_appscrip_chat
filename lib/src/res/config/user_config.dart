7924aa2ece69e1e4a95b64d75fe0c15ac19a4742
Rahul Saryam <rahulsaryam@appscrip.co>
2023-05-18 11:38:48 +0530

    feat(add field for image url)

--- lib/src/res/config/user_config.dart
+++ lib/src/res/config/user_config.dart
@@ -2,7 +2,9 @@ class IsmChatUserConfig {
   const IsmChatUserConfig({
     required this.userToken,
     required this.userId,
+    this.imageUrl,
   });
   final String userToken;
   final String userId;
+  final String? imageUrl;
 }
