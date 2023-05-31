c816766f499a50e9b75961f387542d800dac2fa1
Jatin <jatin@appscrip.co>
2023-05-31 16:57:09 +0530

    feat: reaction overlay - reaction overlay is displayed - overlay is being closed

--- lib/src/views/chat_page/widget/messages/message_bubble.dart
+++ lib/src/views/chat_page/widget/messages/message_bubble.dart
@@ -1,52 +1,111 @@
 import 'package:appscrip_chat_component/appscrip_chat_component.dart';
 import 'package:flutter/material.dart';
+import 'package:focused_menu/focused_menu.dart';
+import 'package:focused_menu/modals.dart';
 import 'package:get/get.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
+import 'package:swipe_to/swipe_to.dart';
 
-class MessageBubble extends StatefulWidget {
+class MessageBubble extends StatelessWidget {
   MessageBubble({
     super.key,
-    required this.message,
     required this.showMessageInCenter,
+    required this.message,
+    required this.index,
   }) : controller = Get.find<IsmChatPageController>();
 
-  final IsmChatMessageModel message;
   final bool showMessageInCenter;
+  final IsmChatMessageModel message;
   final IsmChatPageController controller;
+  final int index;
 
   @override
-  State<MessageBubble> createState() => _MessageBubbleState();
-}
-
-class _MessageBubbleState extends State<MessageBubble> {
-  @override
-  Widget build(BuildContext context) => Container(
-        padding: IsmChatDimens.edgeInsets4,
-        constraints: widget.showMessageInCenter
-            ? BoxConstraints(
-                maxWidth: context.width * .85,
-                minWidth: context.width * .1,
-              )
-            : BoxConstraints(
-                maxWidth: context.width * .8,
-                minWidth: context.width * .1,
-              ),
-        decoration: widget.showMessageInCenter
+  Widget build(BuildContext context) => SwipeTo(
+        offsetDx: showMessageInCenter ? 0 : 0.8,
+        animationDuration: IsmChatConstants.swipeDuration,
+        iconColor: IsmChatConfig.chatTheme.primaryColor,
+        iconSize: 24,
+        onLeftSwipe: showMessageInCenter || !message.sentByMe
             ? null
-            : BoxDecoration(
-                color: widget.message.sentByMe
-                    ? IsmChatConfig.chatTheme.primaryColor
-                    : IsmChatConfig.chatTheme.backgroundColor,
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(IsmChatDimens.twelve),
-                  topLeft: widget.message.sentByMe
-                      ? Radius.circular(IsmChatDimens.twelve)
-                      : Radius.circular(IsmChatDimens.four),
-                  bottomLeft: Radius.circular(IsmChatDimens.twelve),
-                  bottomRight: widget.message.sentByMe
-                      ? Radius.circular(IsmChatDimens.four)
-                      : Radius.circular(IsmChatDimens.twelve),
+            : () {
+                // this works for my messages
+                controller.isreplying = true;
+                controller.chatMessageModel = message;
+              },
+        onRightSwipe: showMessageInCenter || message.sentByMe
+            ? null
+            : () {
+                // this works for opponent message
+                controller.isreplying = true;
+                controller.chatMessageModel = message;
+              },
+        child: FocusedMenuHolder(
+          openWithTap: showMessageInCenter ? true : false,
+          menuWidth: 170,
+          menuOffset: IsmChatDimens.twenty,
+          blurSize: 3,
+          animateMenuItems: false,
+          blurBackgroundColor: Colors.grey,
+          onPressed: () {},
+          menuItems: IsmChatFocusMenuType.values
+              .where((e) =>
+                  e == IsmChatFocusMenuType.info ? message.sentByMe : true)
+              .toList()
+              .map(
+                (e) => FocusedMenuItem(
+                  title: Text(
+                    e.toString(),
+                    style: IsmChatStyles.w400Black12,
+                  ),
+                  onPressed: () => controller.onMenuItemSelected(e, message),
+                  trailingIcon: Icon(e.icon),
                 ),
+              )
+              .toList(),
+          child: IsmChatTapHandler(
+            onTap: () async {
+              if (message.messageType == IsmChatMessageType.reply) {
+                controller.scrollToMessage(message.parentMessageId ?? '');
+              } else {
+                controller.tapForMediaPreview(message);
+              }
+            },
+            child: AutoScrollTag(
+              controller: controller.messagesScrollController,
+              index: index,
+              key: Key('scroll-${message.messageId}'),
+              child: Container(
+                padding: IsmChatDimens.edgeInsets4,
+                constraints: showMessageInCenter
+                    ? BoxConstraints(
+                        maxWidth: context.width * .85,
+                        minWidth: context.width * .1,
+                      )
+                    : BoxConstraints(
+                        maxWidth: context.width * .8,
+                        minWidth: context.width * .1,
+                      ),
+                decoration: showMessageInCenter
+                    ? null
+                    : BoxDecoration(
+                        color: message.sentByMe
+                            ? IsmChatConfig.chatTheme.primaryColor
+                            : IsmChatConfig.chatTheme.backgroundColor,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(IsmChatDimens.twelve),
+                          topLeft: message.sentByMe
+                              ? Radius.circular(IsmChatDimens.twelve)
+                              : Radius.circular(IsmChatDimens.four),
+                          bottomLeft: Radius.circular(IsmChatDimens.twelve),
+                          bottomRight: message.sentByMe
+                              ? Radius.circular(IsmChatDimens.four)
+                              : Radius.circular(IsmChatDimens.twelve),
+                        ),
+                      ),
+                child: message.customType!.messageType(message),
               ),
-        child: widget.message.customType!.messageType(widget.message),
+            ),
+          ),
+        ),
       );
 }
