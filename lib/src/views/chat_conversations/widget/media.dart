6891e8d2b0191d0c435ddc83a20ae59f221be16a
GulabSingh1 <gulabsingh@appscrip.co>
2023-06-14 17:27:28 +0530

    fix conversation info media gallary ui, add date

--- lib/src/views/chat_conversations/widget/media.dart
+++ lib/src/views/chat_conversations/widget/media.dart
@@ -1,12 +1,83 @@
+import 'dart:developer';
+
 import 'package:appscrip_chat_component/appscrip_chat_component.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 /// IsmMedia class is for showing the conversation media
-class IsmMedia extends StatelessWidget {
+class IsmMedia extends StatefulWidget {
   IsmMedia({Key? key, required this.mediaList}) : super(key: key);
 
   List<IsmChatMessageModel> mediaList;
+
+  @override
+  State<IsmMedia> createState() => _IsmMediaState();
+}
+
+class _IsmMediaState extends State<IsmMedia> {
+
+  List<IsmChatMessageModel> storeMedia = [];
+
+  @override
+  void initState(){
+    var x = sortMessages(widget.mediaList);
+    sortMediaList(x);
+    super.initState();
+  }
+
+  List<Map<String, List<IsmChatMessageModel>>> sortMediaList (List<IsmChatMessageModel> messages){
+    var storeMediaImageList = [];
+    for (var x in messages) {
+      if(x.customType == IsmChatCustomMessageType.date){
+          storeMediaImageList.add({x.body : <IsmChatMessageModel>[]});
+          continue;
+      }
+        var z =storeMediaImageList.last as Map<String,List<dynamic>>;
+        z.forEach((key, value) {
+            value.add(x);
+        });
+    }
+    return [];
+  }
+
+  List<IsmChatMessageModel> sortMessages(List<IsmChatMessageModel> messages) {
+    messages.sort((a, b) => a.sentAt.compareTo(b.sentAt));
+
+    return _parseMessagesWithDate(messages);
+  }
+
+  List<IsmChatMessageModel> _parseMessagesWithDate(
+      List<IsmChatMessageModel> messages,
+      ) {
+    var result = <List<IsmChatMessageModel>>[];
+    var list1 = <IsmChatMessageModel>[];
+    var allMessages = <IsmChatMessageModel>[];
+    for (var x = 0; x < messages.length; x++) {
+      if (x == 0) {
+        list1.add(messages[x]);
+      } else if (DateTime.fromMillisecondsSinceEpoch(messages[x - 1].sentAt)
+          .isSameDay(DateTime.fromMillisecondsSinceEpoch(messages[x].sentAt))) {
+        list1.add(messages[x]);
+      } else {
+        result.add([...list1]);
+        list1.clear();
+        list1.add(messages[x]);
+      }
+      if (x == messages.length - 1 && list1.isNotEmpty) {
+        result.add([...list1]);
+      }
+    }
+
+    for (var messages in result) {
+      allMessages.add(
+        IsmChatMessageModel.fromMonth(
+          messages.first.sentAt,
+        ),
+      );
+      allMessages.addAll(messages);
+    }
+    return allMessages;
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -21,7 +92,7 @@ class IsmMedia extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: IsmChatDimens.edgeInsets10,
-            child: mediaList.isEmpty
+            child: widget.mediaList.isEmpty
                 ? Center(
                     child: Text(
                       IsmChatStrings.noMedia,
@@ -29,7 +100,7 @@ class IsmMedia extends StatelessWidget {
                     ),
                   )
                 : GridView.builder(
-                    itemCount: mediaList.length,
+                    itemCount: widget.mediaList.length,
                     gridDelegate:
                         const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3,
@@ -37,7 +108,7 @@ class IsmMedia extends StatelessWidget {
                       crossAxisSpacing: 8,
                     ),
                     itemBuilder: (context, index) {
-                      var media = mediaList[index];
+                      var media = widget.mediaList[index];
                       var url =
                           media.customType == IsmChatCustomMessageType.image
                               ? media.attachments!.first.mediaUrl!
