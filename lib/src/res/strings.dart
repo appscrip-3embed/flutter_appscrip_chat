0b4e4a9872773c55c5379269eecf4f011163f90c
Rahul Saryam <rahulsaryam@appscrip.co>
2023-06-25 22:56:43 +0530

    fix(profile issues)

--- lib/src/res/strings.dart
+++ lib/src/res/strings.dart
@@ -92,7 +92,7 @@ class IsmChatStrings {
   static const String loremIpsum =
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod';
   static const String demoDate = '25 Dec 2016';
-  static const String mediaLinksAndDocs = 'Media, Links, and Docs';
+  static const String mediaLinksAndDocs = 'Media, Links and Docs';
   static const String links = 'Links';
   static const String docs = 'Docs';
   static const String readby = 'Read by';
--- lib/src/views/chat_page/widget/user_info.dart
+++ lib/src/views/chat_page/widget/user_info.dart
@@ -3,128 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
-// class IsmChatUserInfo extends StatefulWidget {
-//   const IsmChatUserInfo({super.key, required this.userDetails});
-
-//   final UserDetails userDetails;
-
-//   @override
-//   State<IsmChatUserInfo> createState() => _IsmChatUserInfoState();
-// }
-
-// class _IsmChatUserInfoState extends State<IsmChatUserInfo> {
-//   final chatController = Get.find<IsmChatPageController>();
-//   final conversationController = Get.find<IsmChatConversationsController>();
-//   bool isUserBlock = false;
-
-//   @override
-//   void initState() {
-//     if (!conversationController.blockUsers.isNullOrEmpty) {
-//       isUserBlock = conversationController.blockUsers
-//           .any((e) => e.userId == widget.userDetails.userId);
-//     }
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) => SizedBox(
-//         height: IsmChatDimens.twoHundred + IsmChatDimens.oneHundredFifty,
-//         width: IsmChatDimens.twoHundred + IsmChatDimens.eighty,
-//         child: Stack(
-//           alignment: Alignment.topRight,
-//           children: [
-//             Column(
-//               mainAxisSize: MainAxisSize.min,
-//               children: [
-//                 SizedBox(
-//                   height: IsmChatDimens.twoHundred + IsmChatDimens.fifty,
-//                   child: CachedNetworkImage(
-//                     imageUrl: widget.userDetails.profileUrl,
-//                     fit: BoxFit.fill,
-//                     alignment: Alignment.center,
-//                     imageBuilder: (_, image) => Container(
-//                       decoration: BoxDecoration(
-//                         borderRadius: const BorderRadius.only(
-//                             topLeft: Radius.circular(10),
-//                             topRight: Radius.circular(10)),
-//                         color: IsmChatConfig.chatTheme.backgroundColor!,
-//                         image: DecorationImage(image: image, fit: BoxFit.cover),
-//                       ),
-//                     ),
-//                     placeholder: (context, url) => Container(
-//                       alignment: Alignment.center,
-//                       decoration: BoxDecoration(
-//                         color: IsmChatConfig.chatTheme.primaryColor!
-//                             .withOpacity(0.2),
-//                       ),
-//                       child: const Center(
-//                         child: CircularProgressIndicator.adaptive(),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//                 IsmChatDimens.boxHeight5,
-//                 Text(
-//                   widget.userDetails.userName,
-//                   style: IsmChatStyles.w600Black20,
-//                 ),
-//                 Text(
-//                   widget.userDetails.userIdentifier,
-//                   style: IsmChatStyles.w600Grey16,
-//                 ),
-//                 IsmChatDimens.boxHeight5,
-//                 IsmChatTapHandler(
-//                   onTap: () async {
-//                     await Get.dialog(IsmChatAlertDialogBox(
-//                       title: isUserBlock
-//                           ? IsmChatStrings.doWantUnBlckUser
-//                           : IsmChatStrings.doWantBlckUser,
-//                       actionLabels: [
-//                         isUserBlock
-//                             ? IsmChatStrings.unblock
-//                             : IsmChatStrings.block,
-//                       ],
-//                       callbackActions: [
-//                         () {
-//                           Get.back();
-//                           isUserBlock
-//                               ? chatController.unblockUser(
-//                                   opponentId: widget.userDetails.userId,
-//                                   lastMessageTimeStamp: 0,
-//                                   fromUser: true,
-//                                 )
-//                               : chatController.blockUser(
-//                                   opponentId: widget.userDetails.userId,
-//                                   lastMessageTimeStamp: 0,
-//                                   fromUser: true,
-//                                 );
-//                         },
-//                       ],
-//                     ));
-//                   },
-//                   child: Text(
-//                     isUserBlock
-//                         ? IsmChatStrings.unBlockUser
-//                         : IsmChatStrings.blockUser,
-//                     style: IsmChatStyles.w600red16,
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             IconButton(
-//                 onPressed: Get.back,
-//                 icon: CircleAvatar(
-//                   radius: IsmChatDimens.twelve,
-//                   child: const Icon(
-//                     Icons.clear_rounded,
-//                     color: IsmChatColors.blackColor,
-//                   ),
-//                 ))
-//           ],
-//         ),
-//       );
-// }
-
 class IsmChatUserInfo extends StatefulWidget {
   const IsmChatUserInfo(
       {super.key, required this.user, required this.conversationId});
@@ -165,14 +43,12 @@ class _IsmChatUserInfoState extends State<IsmChatUserInfo> {
           .where((e) => [
                 IsmChatCustomMessageType.video,
                 IsmChatCustomMessageType.image,
-                IsmChatCustomMessageType.file,
-                IsmChatCustomMessageType.audio,
-                IsmChatCustomMessageType.link,
+                IsmChatCustomMessageType.audio
               ].contains(e.customType))
           .toList();
       mediaListLinks = messages
           .where((e) => [
-                IsmChatCustomMessageType.audio,
+                IsmChatCustomMessageType.link,
               ].contains(e.customType))
           .toList();
       mediaListDocs = messages
@@ -180,6 +56,7 @@ class _IsmChatUserInfoState extends State<IsmChatUserInfo> {
                 IsmChatCustomMessageType.file,
               ].contains(e.customType))
           .toList();
+      setState(() {});
     }
   }
 
@@ -188,7 +65,7 @@ class _IsmChatUserInfoState extends State<IsmChatUserInfo> {
         backgroundColor: IsmChatColors.whiteColor,
         appBar: IsmChatAppBar(
           title: Text(
-            'User info',
+            IsmChatStrings.contactInfo,
             style: IsmChatStyles.w600White18,
           ),
         ),
@@ -203,7 +80,7 @@ class _IsmChatUserInfoState extends State<IsmChatUserInfo> {
                   IsmChatDimens.boxHeight16,
                   IsmChatImage.profile(
                     widget.user.profileUrl,
-                    dimensions: IsmChatDimens.hundred,
+                    dimensions: IsmChatDimens.oneHundredFifty,
                   ),
                   IsmChatDimens.boxHeight5,
                   Text(
@@ -260,46 +137,48 @@ class _IsmChatUserInfoState extends State<IsmChatUserInfo> {
                       ),
                     ),
                   ),
-                  TextButton.icon(
-                    onPressed: () async {
-                      await Get.dialog(IsmChatAlertDialogBox(
-                        title: isUserBlock
-                            ? IsmChatStrings.doWantUnBlckUser
-                            : IsmChatStrings.doWantBlckUser,
-                        actionLabels: [
-                          isUserBlock
-                              ? IsmChatStrings.unblock
-                              : IsmChatStrings.block,
-                        ],
-                        callbackActions: [
-                          () {
-                            Get.back();
+                  if (IsmChatConfig.communicationConfig.userConfig.userId !=
+                      widget.user.userId)
+                    TextButton.icon(
+                      onPressed: () async {
+                        await Get.dialog(IsmChatAlertDialogBox(
+                          title: isUserBlock
+                              ? IsmChatStrings.doWantUnBlckUser
+                              : IsmChatStrings.doWantBlckUser,
+                          actionLabels: [
                             isUserBlock
-                                ? chatPageController.unblockUser(
-                                    opponentId: widget.user.userId,
-                                    lastMessageTimeStamp: 0,
-                                    fromUser: true,
-                                  )
-                                : chatPageController.blockUser(
-                                    opponentId: widget.user.userId,
-                                    lastMessageTimeStamp: 0,
-                                    fromUser: true,
-                                  );
-                          },
-                        ],
-                      ));
-                    },
-                    icon: const Icon(
-                      Icons.block_rounded,
-                      color: IsmChatColors.redColor,
+                                ? IsmChatStrings.unblock
+                                : IsmChatStrings.block,
+                          ],
+                          callbackActions: [
+                            () {
+                              Get.back();
+                              isUserBlock
+                                  ? chatPageController.unblockUser(
+                                      opponentId: widget.user.userId,
+                                      lastMessageTimeStamp: 0,
+                                      fromUser: true,
+                                    )
+                                  : chatPageController.blockUser(
+                                      opponentId: widget.user.userId,
+                                      lastMessageTimeStamp: 0,
+                                      fromUser: true,
+                                    );
+                            },
+                          ],
+                        ));
+                      },
+                      icon: const Icon(
+                        Icons.block_rounded,
+                        color: IsmChatColors.redColor,
+                      ),
+                      label: Text(
+                        isUserBlock
+                            ? IsmChatStrings.unblock
+                            : IsmChatStrings.block,
+                        style: IsmChatStyles.w600red16,
+                      ),
                     ),
-                    label: Text(
-                      isUserBlock
-                          ? IsmChatStrings.unblock
-                          : IsmChatStrings.block,
-                      style: IsmChatStyles.w600red16,
-                    ),
-                  ),
                 ],
               ),
             ),
