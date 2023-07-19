bfa534d70449095ab5d08dc6eef203404ec08c86
Rahul Saryam <rahulsaryam@appscrip.co>
2023-07-19 19:05:27 +0530

    fix(background color and decoration for chat page)

--- example/lib/views/chat_message_view.dart
+++ example/lib/views/chat_message_view.dart
@@ -11,15 +11,6 @@ class ChatMessageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IsmChatPageView(
-      header: IsmChatHeader(
-          // shape: BeveledRectangleBorder(
-          //   borderRadius: BorderRadius.vertical(
-          //     bottom: Radius.circular(
-          //       IsmChatDimens.twenty,
-          //     ),
-          //   ),
-          // ),
-          ),
       emptyChatPlaceholder: IsmChatEmptyView(
         icon: Icon(
           Icons.chat_outlined,
