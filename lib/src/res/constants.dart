48eb9441dd90c480eff7bc32c795b2b5b436caf0
Rahul Saryam <rahulsaryam@appscrip.co>
2023-08-22 16:12:04 +0530

    fix(attachment issues)

--- lib/src/res/constants.dart
+++ lib/src/res/constants.dart
@@ -30,6 +30,10 @@ class IsmChatConstants {
 
   static const int attachmentShowLine = 3;
 
+  static const int attachmentHight = 130;
+
+  static const int attachmentShowLine = 3;
+
   static const String mapAPIKey = 'AIzaSyC2YXqs5H8QSfN1NVsZKsP11XLZhfGVGPI';
 
   /// Video Type List For Every Platform
