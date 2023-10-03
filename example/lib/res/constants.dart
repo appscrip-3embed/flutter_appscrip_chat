2ac4dd4c274318133b5a1bbc870f70be415e324e
Rahul Saryam <rahulsaryam@appscrip.co>
2023-10-03 18:08:27 +0530

    feat(update version no for andriod)

--- example/lib/res/constants.dart
+++ example/lib/res/constants.dart
@@ -1,34 +1,34 @@
-class Constants {
-  const Constants._();
-
-  static const String licenseKey = 'lic-IMKw4CVRR6IHZ26+FhU7S+eG2Egi9NIHjMB';
-  static const String appSecret =
-      'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDYwYjFlZjcwZmZjZjc1MDAwMWQwNDQwZG0AAAAIa2V5c2V0SWRtAAAAJDJmN2QyMmQ3LWI3NDMtNDcxNC04MzdlLWFmMGIxMjliZDU0Mm0AAAAJcHJvamVjdElkbQAAACRmYzRiZmMwOC0zMjQzLTQyMGYtODg0ZC0xY2UzN2QzZjQ1M2JkAAZzaWduZWRuBgCTqH3aiQE.mfI7oJEXiUoUbB5BodtHliEph-1rzDnUyZgL9zFBaGg';
-  static const String userSecret =
-      'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDYwYjFlZjcwZmZjZjc1MDAwMWQwNDQwZG0AAAAIa2V5c2V0SWRtAAAAJDJmN2QyMmQ3LWI3NDMtNDcxNC04MzdlLWFmMGIxMjliZDU0Mm0AAAAJcHJvamVjdElkbQAAACRmYzRiZmMwOC0zMjQzLTQyMGYtODg0ZC0xY2UzN2QzZjQ1M2JkAAZzaWduZWRuBgCaqH3aiQE.h7vcNEDO3dG6lgfh8i3UdfhZ3bOpsmklCOALLmY4KhM';
-  static const String accountId = '60b1ef70ffcf750001d0440d';
-  static const String keysetId = '2f7d22d7-b743-4714-837e-af0b129bd542';
-  static const String projectId = 'fc4bfc08-3243-420f-884d-1ce37d3f453b';
-
-  static const String hostname = 'connections.isometrik.io';
-  static const int port = 2052;
-}
-
 // class Constants {
 //   const Constants._();
 
-//   static const String licenseKey = 'lic-IMKT8jm9Ac7sIu1GBYGeo7mzdt5Yr0sRs9s';
+//   static const String licenseKey = 'lic-IMKw4CVRR6IHZ26+FhU7S+eG2Egi9NIHjMB';
 //   static const String appSecret =
-//       'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDY1MTEyZmMxMmNhM2JmMzZkMmQ1ZTY3OW0AAAAIa2V5c2V0SWRtAAAAJDhlZjdiODRiLTNkYjQtNGE1MC05ZGVmLTAxODE2ZTdjZDc3NW0AAAAJcHJvamVjdElkbQAAACQ4YWU4NTNjMi02NzAwLTQ0ZjktOWMyNi01NTNmZmVjYzg2YTdkAAZzaWduZWRuBgB7mDLLigE.OktQUZNww9Yw2a3Hn29duFxFPWylmWd2QkXj3hL_40c';
+//       'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDYwYjFlZjcwZmZjZjc1MDAwMWQwNDQwZG0AAAAIa2V5c2V0SWRtAAAAJDJmN2QyMmQ3LWI3NDMtNDcxNC04MzdlLWFmMGIxMjliZDU0Mm0AAAAJcHJvamVjdElkbQAAACRmYzRiZmMwOC0zMjQzLTQyMGYtODg0ZC0xY2UzN2QzZjQ1M2JkAAZzaWduZWRuBgCTqH3aiQE.mfI7oJEXiUoUbB5BodtHliEph-1rzDnUyZgL9zFBaGg';
 //   static const String userSecret =
-//       'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDY1MTEyZmMxMmNhM2JmMzZkMmQ1ZTY3OW0AAAAIa2V5c2V0SWRtAAAAJDhlZjdiODRiLTNkYjQtNGE1MC05ZGVmLTAxODE2ZTdjZDc3NW0AAAAJcHJvamVjdElkbQAAACQ4YWU4NTNjMi02NzAwLTQ0ZjktOWMyNi01NTNmZmVjYzg2YTdkAAZzaWduZWRuBgB7mDLLigE.hZnsDAw5_MExjV2KEZu_uOJjfCshX4XdnQdEn3EPgPg';
-//   static const String accountId = '65112fc12ca3bf36d2d5e679';
-//   static const String keysetId = '8ef7b84b-3db4-4a50-9def-01816e7cd775';
-//   static const String projectId = '8ae853c2-6700-44f9-9c26-553ffecc86a7';
+//       'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDYwYjFlZjcwZmZjZjc1MDAwMWQwNDQwZG0AAAAIa2V5c2V0SWRtAAAAJDJmN2QyMmQ3LWI3NDMtNDcxNC04MzdlLWFmMGIxMjliZDU0Mm0AAAAJcHJvamVjdElkbQAAACRmYzRiZmMwOC0zMjQzLTQyMGYtODg0ZC0xY2UzN2QzZjQ1M2JkAAZzaWduZWRuBgCaqH3aiQE.h7vcNEDO3dG6lgfh8i3UdfhZ3bOpsmklCOALLmY4KhM';
+//   static const String accountId = '60b1ef70ffcf750001d0440d';
+//   static const String keysetId = '2f7d22d7-b743-4714-837e-af0b129bd542';
+//   static const String projectId = 'fc4bfc08-3243-420f-884d-1ce37d3f453b';
 
 //   static const String hostname = 'connections.isometrik.io';
 //   static const int port = 2052;
 // }
+
+class Constants {
+  const Constants._();
+
+  static const String licenseKey = 'lic-IMKTct7X204nyaxMAhaLATkyBOuqX3gYRbQ';
+  static const String appSecret =
+      'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDY1MWJmZmI2ZWFiNTk0Mzk1NDlhMjYxN20AAAAIa2V5c2V0SWRtAAAAJDM0YTYxYzE1LWI2NzktNDdiMy1iYTUxLTliOGRlMTE4NzJlMm0AAAAJcHJvamVjdElkbQAAACRlZGM3ZDhlMC1mOWMyLTQ5M2MtOGU5MC1iMGU3MDAwYjk3YjhkAAZzaWduZWRuBgDKGGD1igE.QgkjYUPYhfCO-Qpi9gXMoInYTmX9P6BXUaJPmBRqFsQ';
+  static const String userSecret =
+      'SFMyNTY.g3QAAAACZAAEZGF0YXQAAAADbQAAAAlhY2NvdW50SWRtAAAAGDY1MWJmZmI2ZWFiNTk0Mzk1NDlhMjYxN20AAAAIa2V5c2V0SWRtAAAAJDM0YTYxYzE1LWI2NzktNDdiMy1iYTUxLTliOGRlMTE4NzJlMm0AAAAJcHJvamVjdElkbQAAACRlZGM3ZDhlMC1mOWMyLTQ5M2MtOGU5MC1iMGU3MDAwYjk3YjhkAAZzaWduZWRuBgDKGGD1igE.PZptAQF2CujmjEQEWk4YMYwje-Ac3uBTGKOKEnI14As';
+  static const String accountId = '651bffb6eab59439549a2617';
+  static const String keysetId = '34a61c15-b679-47b3-ba51-9b8de11872e2';
+  static const String projectId = 'edc7d8e0-f9c2-493c-8e90-b0e7000b97b8';
+
+  static const String hostname = 'connections.isometrik.io';
+  static const int port = 2052;
+}
 
 class AssetConstants {
   const AssetConstants._();
