bc643d8202b9224bc1cbdd72fce07431a5992847
rahulsaryam <rahulsaryam@appscrip.co>
2023-10-09 18:40:03 +0530

    inprogress(push notificaiton)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:appscrip_chat_component/appscrip_chat_component.dart';
 import 'package:chat_component_example/res/res.dart';
 import 'package:chat_component_example/utilities/utilities.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -28,8 +29,46 @@ Future<void> initialize() async {
   setPathUrlStrategy();
   if (!kIsWeb) {
     await Firebase.initializeApp();
-  }
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgorundHandler);
 
+    // when app is killed
+    final initialMessage = await messaging.getInitialMessage();
+    IsmChatLog.error('recieved messgae on app killed step1');
+    if (initialMessage != null) {
+      IsmChatLog.error('recieved messgae on app killed step2');
+      LocalNoticeService().cancelAllNotification();
+      LocalNoticeService().addNotification(
+        '', // Add the  sender user name here
+        '', // MessageName
+        DateTime.now().millisecondsSinceEpoch + 1 * 1000,
+        sound: '',
+        channel: 'message',
+      );
+    }
+
+    // when app is in background
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? message) async {
+      IsmChatLog.error('recieved messgae on app background');
+      LocalNoticeService().cancelAllNotification();
+      LocalNoticeService().addNotification(
+        '', // Add the  sender user name here
+        '', // MessageName
+        DateTime.now().millisecondsSinceEpoch + 1 * 1000,
+        sound: '',
+        channel: 'message',
+      );
+    });
+  }
   dbWrapper = await DBWrapper.create();
   await AppConfig.getUserData();
   await Future.wait(
@@ -38,6 +77,12 @@ Future<void> initialize() async {
       LocalNoticeService().setup(),
     ],
   );
+}
+
+/// Call this funcation for get notifcaiton when app killed
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgorundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatefulWidget {
--- lib/src/utilities/notifications/local_notice_service.dart
+++ lib/src/utilities/notifications/local_notice_service.dart
@@ -38,6 +38,7 @@
 ///
 ///
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 // #1
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
