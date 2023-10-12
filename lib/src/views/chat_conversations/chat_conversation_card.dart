0d9125f673600a53637eeb3c80898e82a3449a45
rahulsaryam <rahulsaryam@appscrip.co>
2023-10-12 14:26:02 +0530

    fix(issues)

--- lib/src/views/chat_conversations/chat_conversation_card.dart
+++ lib/src/views/chat_conversations/chat_conversation_card.dart
@@ -1,6 +1,5 @@
 import 'package:appscrip_chat_component/appscrip_chat_component.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class IsmChatConversationCard extends StatefulWidget {
   const IsmChatConversationCard(
@@ -123,13 +122,13 @@ class _IsmChatConversationCardState extends State<IsmChatConversationCard>
                           ),
                     ),
                   ],
-                  if (widget.conversation.conversationType ==
-                      IsmChatConversationType.public) ...[
-                    Positioned(
-                        bottom: IsmChatDimens.zero,
-                        right: IsmChatDimens.zero,
-                        child: SvgPicture.asset(IsmChatAssets.publicGroupSvg))
-                  ]
+                  // if (widget.conversation.conversationType ==
+                  //     IsmChatConversationType.public) ...[
+                  //   Positioned(
+                  //       bottom: IsmChatDimens.zero,
+                  //       right: IsmChatDimens.zero,
+                  //       child: SvgPicture.asset(IsmChatAssets.publicGroupSvg))
+                  // ]
                 ],
               ),
             ),
