c5c044df305c27fc8a09c9ee76a1dc992eefeec4
rahulsaryam <rahulsaryam@appscrip.co>
2024-03-20 11:36:11 +0530

    fix(alice issues for web)

--- lib/src/data/network/chat_api_wrapper.dart
+++ lib/src/data/network/chat_api_wrapper.dart
@@ -4,12 +4,19 @@ import 'dart:convert';
 import 'package:alice/alice.dart';
 import 'package:alice/core/alice_http_extensions.dart';
 import 'package:appscrip_chat_component/appscrip_chat_component.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class IsmChatApiWrapper {
+  final alice = kIsWeb
+      ? Alice()
+      : Alice(
+          showNotification: kDebugMode,
+          showInspectorOnShake: kDebugMode,
+          showShareButton: kDebugMode,
+        );
 
-  
   Future<IsmChatResponseModel> _handleNoInternet(bool showDailog) async {
     IsmChatLog.error('----- Internet not working -----');
     if (showDailog && Get.isDialogOpen == false) {
@@ -40,7 +47,7 @@ class IsmChatApiWrapper {
     try {
       final response = await http
           .get(uri, headers: headers)
-          .interceptWithAlice(IsmChatConfig.useAlice ?? Alice())
+          .interceptWithAlice(IsmChatConfig.useAlice ?? alice)
           .timeout(const Duration(seconds: 60));
 
       if (showLoader) {
@@ -82,7 +89,7 @@ class IsmChatApiWrapper {
             body: jsonEncode(payload),
             headers: headers,
           )
-          .interceptWithAlice(IsmChatConfig.useAlice ?? Alice(),
+          .interceptWithAlice(IsmChatConfig.useAlice ?? alice,
               body: jsonEncode(payload))
           .timeout(const Duration(seconds: 60));
 
@@ -126,7 +133,7 @@ class IsmChatApiWrapper {
             body: forAwsUpload ? payload : jsonEncode(payload),
             headers: headers,
           )
-          .interceptWithAlice(IsmChatConfig.useAlice ?? Alice(),
+          .interceptWithAlice(IsmChatConfig.useAlice ?? alice,
               body: forAwsUpload ? payload : jsonEncode(payload))
           .timeout(
             const Duration(seconds: 60),
@@ -171,7 +178,7 @@ class IsmChatApiWrapper {
             body: jsonEncode(payload),
             headers: headers,
           )
-          .interceptWithAlice(IsmChatConfig.useAlice ?? Alice(),
+          .interceptWithAlice(IsmChatConfig.useAlice ?? alice,
               body: jsonEncode(payload))
           .timeout(const Duration(seconds: 60));
 
@@ -214,7 +221,7 @@ class IsmChatApiWrapper {
             body: jsonEncode(payload),
             headers: headers,
           )
-          .interceptWithAlice(IsmChatConfig.useAlice ?? Alice(),
+          .interceptWithAlice(IsmChatConfig.useAlice ??alice,
               body: jsonEncode(payload))
           .timeout(const Duration(seconds: 60));
 
