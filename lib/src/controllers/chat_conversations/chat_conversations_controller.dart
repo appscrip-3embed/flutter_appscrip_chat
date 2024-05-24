3c5e6783dc659f87490a0a961db36173cb26f03f
RAHUL SARYAM <81813621+rahulsaryam285@users.noreply.github.com>
2024-05-24 17:21:02 +0530

    fix(dependency issues)

--- lib/src/controllers/chat_conversations/chat_conversations_controller.dart
+++ lib/src/controllers/chat_conversations/chat_conversations_controller.dart
@@ -303,7 +303,7 @@ class IsmChatConversationsController extends GetxController {
   /// This variable use for store streamSubscription
   ///
   /// This StreamSubscription listen internet `on` or `off` when app in running
-  StreamSubscription<List<ConnectivityResult>>? connectivitySubscription;
+  StreamSubscription<ConnectivityResult>? connectivitySubscription;
 
   /// This variable use for store user messages which is get from local db
 
@@ -384,6 +384,7 @@ class IsmChatConversationsController extends GetxController {
     connectivity = Connectivity();
     connectivitySubscription =
         connectivity?.onConnectivityChanged.listen((event) async {
+        
       _sendPendingMessage();
     });
   }
