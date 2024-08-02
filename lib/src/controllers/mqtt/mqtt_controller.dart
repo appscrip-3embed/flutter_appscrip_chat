ced3907a7fd171cf16136faa4389515a4de3043f
RAHUL SARYAM <rahulsaryam@appscrip.co>
2024-08-02 13:05:39 +0530

    fix:remove onData callback from mqttcontroller

--- lib/src/controllers/mqtt/mqtt_controller.dart
+++ lib/src/controllers/mqtt/mqtt_controller.dart
@@ -87,12 +87,6 @@ class IsmChatMqttController extends GetxController with IsmChatMqttEventMixin {
         await onMqttEvent(event: event);
       },
     );
-    mqttHelper.onData(
-      (event) async {
-        IsmChatLog.info('Mqtt event $event');
-        // await onMqttEvent(event: event);
-      },
-    );
   }
 
   /// onConnected callback, it will be called when connection is established
