68b30fb504ae34f5d62aac8919cb244ca2b6a8d4
RAHUL SARYAM <rahulsaryam@appscrip.co>
2024-08-07 22:50:00 +0530

    fix: pod update

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 import GoogleMaps
 import flutter_local_notifications
 import FirebaseCore
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
