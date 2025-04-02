cb67457c846c7c2269d659ad82182a86d34f9cd4
RAHUL SARYAM <rahulsaryam@appscrip.co>
2025-04-02 18:40:02 +0530

    fix: change conversation created message

--- lib/src/views/chat_page/widget/messages/conversation_created_message.dart
+++ lib/src/views/chat_page/widget/messages/conversation_created_message.dart
@@ -19,7 +19,7 @@ class IsmChatConversationCreatedMessage extends StatelessWidget {
           child: Text(
             message.isGroup == true
                 ? '${message.userName} created a group'
-                : 'Messages are end to end encrypted. No one outside of this chat can read to them.',
+                : 'Report / block unwanted solicitation by clicking the 3 dots in the top right corner.', // 'Messages are end to end encrypted. No one outside of this chat can read to them.',
             style: IsmChatConfig.chatTheme.chatPageTheme?.centerMessageThemData
                     ?.textStyle ??
                 IsmChatStyles.w500Black12.copyWith(
